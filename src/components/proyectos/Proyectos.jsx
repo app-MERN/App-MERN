aed974d44836cb8c2c14a7145cc3224d741216a9
Alexander Ayra <alexander.ayras@gmail.com>
2020-09-18 19:53:59 -0500

    Primeros Pasos

--- src/components/proyectos/Proyectos.jsx
+++ src/components/proyectos/Proyectos.jsx
@@ -1,9 +1,15 @@
 import React from 'react';
+import Sidebar from '../layout/Sidebar';
 
 const Proyectos = () => {
   return (
-    <div>
-      <h1>Desde Proyectos</h1>
+    <div className='contenedor-app'>
+      <Sidebar />
+      <div className='seccion-principal'>
+        <main>
+          <div className='contenedor-tareas' />
+        </main>
+      </div>
     </div>
   );
 };
