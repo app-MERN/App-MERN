059f8b0252c614eecc26df6a093ed3162f8049df
Alexander Ayra <alexander.ayras@gmail.com>
2020-09-19 01:05:50 -0500

    Creando el Listado de Tareas

--- src/components/tareas/ListadoTareas.jsx
+++ src/components/tareas/ListadoTareas.jsx
@@ -1,1 +1,31 @@
 import React from 'react';
+import Tarea from './Tarea';
+
+const ListadoTareas = () => {
+  const tareasProyecto = [
+    { nombre: 'Elegir Plataforma', estado: true },
+    { nombre: 'Elegir Colores', estado: false },
+    { nombre: 'Elegir Plataforma de Pago', estado: false },
+    { nombre: 'Elegir Hosting', estado: true },
+  ];
+
+  return (
+    <div>
+      <h2>Proyecto: Tienda Virtual</h2>
+
+      <ul className='listado-tareas'>
+        { tareasProyecto.length === 0
+          ? (<li><p>No hay Tareas</p></li>)
+          : tareasProyecto.map((tarea) => {
+            return (
+              <Tarea
+                tarea={tarea}
+              />
+            );
+          })}
+      </ul>
+    </div>
+  );
+};
+
+export default ListadoTareas;
--- src/components/tareas/Tarea.jsx
+++ src/components/tareas/Tarea.jsx
@@ -1,1 +1,11 @@
 import React from 'react';
+
+const Tarea = () => {
+  return (
+    <div>
+      <h1>Hola Desde Tarea</h1>
+    </div>
+  );
+};
+
+export default Tarea;
