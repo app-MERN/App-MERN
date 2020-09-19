c46e72575ab47e79d3abacfc763333a0bdc102b5
Alexander Ayra <alexander.ayras@gmail.com>
2020-09-18 20:00:17 -0500

    Creando el Formulario para Nuevos Proyectos

--- src/components/layout/Sidebar.jsx
+++ src/components/layout/Sidebar.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import NuevoProyecto from '../proyectos/NuevoProyecto';
 
 const Sidebar = () => {
   return (
@@ -7,6 +8,7 @@ const Sidebar = () => {
         MERN
         <span>Tasks</span>
       </h1>
+      <NuevoProyecto />
       <div className='proyectos'>
         <h2>Tus Proyectos</h2>
       </div>
