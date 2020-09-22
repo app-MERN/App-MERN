637633224a6a97142d8217ef503d3359733fc70e
Alexander Ayra <alexander.ayras@gmail.com>
2020-09-22 12:39:19 -0500

    Trabajando con la Interfaz de Tareas

--- src/components/tareas/Tarea.jsx
+++ src/components/tareas/Tarea.jsx
@@ -1,10 +1,35 @@
 import React from 'react';
 
-const Tarea = () => {
+const Tarea = ({ tarea }) => {
   return (
-    <div>
-      <h1>Hola Desde Tarea</h1>
-    </div>
+    <li className="tarea sombra">
+      <p>{ tarea.nombre }</p>
+
+      <div className="estado">
+        {tarea.estado
+          ? <button
+              type='button'
+              className='completo'
+            >Completo</button>
+          : <button
+              type='button'
+              className='incompleto'
+            >Incompleto</button>
+        }
+      </div>
+
+      <div className="acciones">
+        <button
+          type='button'
+          className='btn btn-primario'
+        >Editar</button>
+
+        <button
+          type='button'
+          className='btn btn-secundario'
+        >Eliminar</button>
+      </div>
+    </li>
   );
 };
 
