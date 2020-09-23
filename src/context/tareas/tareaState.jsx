d3cd9125738f26490359371741e53cce976324b5
Alexander Ayra <alexander.ayras@gmail.com>
2020-09-23 18:16:09 -0500

    Agregando State y Provider para Tareas

---
+++ src/context/tareas/tareaState.jsx
@@ -0,0 +1,17 @@
+import React, { useReducer } from 'react'
+import TareaContext from './tareaContext'
+import TareaReducer from './tareaReducer'
+
+const TareaState = (props) => {
+  const initialState = { tareas: [] }
+
+  const [state, dispatch] = useReducer(TareaReducer, initialState)
+
+  return (
+    <TareaContext.Provider>
+      { props.children }
+    </TareaContext.Provider>
+  )
+}
+
+export default TareaState
