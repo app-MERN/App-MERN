8d77c9e586cef3324241b3b3c41811f15146aa4b
Alexander Ayra <alexander.ayras@gmail.com>
2020-09-24 12:06:27 -0500

    Insertando tareas en el Proyecto Actual

--- src/components/tareas/FormTarea.jsx
+++ src/components/tareas/FormTarea.jsx
@@ -1,16 +1,35 @@
-import React, { useContext } from 'react'
+import React, { useContext, useState } from 'react'
 import proyectoContext from '../../context/proyectos/proyectoContext'
+import TareaContext from '../../context/tareas/tareaContext'
 
 const FormTarea = () => {
   const proyectosContext = useContext(proyectoContext)
   const { proyectoz } = proyectosContext
 
+  const tareasContext = useContext(TareaContext)
+  const { addTarea } = tareasContext
+
+  const [tarea, setTarea] = useState({ nombre: '' })
+
+  const { nombre } = tarea
+
   if (!proyectoz) return null
 
   const [proyectoActual] = proyectoz
 
+  const handleChange = (e) => {
+    setTarea({
+      ...tarea,
+      [e.target.name]: e.target.value,
+    })
+  }
+
   const handleSubmit = (e) => {
     e.preventDefault()
+
+    tarea.proyectoId = proyectoActual.id
+    tarea.estado = false
+    addTarea(tarea)
   }
 
   return (
@@ -26,6 +45,8 @@ const FormTarea = () => {
             className='input-text'
             placeholder='Nombre Tarea...'
             name='nombre'
+            value={nombre}
+            onChange={handleChange}
           />
         </div>
         <div className='contenedor-input'>
